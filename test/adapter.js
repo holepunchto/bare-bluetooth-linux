9ffb038e7e0d73e9489b12418e67c8477669cdbe
Tony Gorez <gorez.tony@gmail.com>
2026-05-07 15:22:30 +0200

    test: clean
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/adapter.js
+++ test/adapter.js
@@ -3,37 +3,32 @@ const { Adapter } = require('..')
 const { isCI } = require('./helpers')
 
 test('constructor sets default path', (t) => {
-  const adapter = new Adapter()
+  using adapter = new Adapter()
   t.is(adapter.path, '/org/bluez/hci0')
-  adapter.destroy()
 })
 
 test('constructor accepts custom path', (t) => {
-  const adapter = new Adapter({ path: '/org/bluez/hci1' })
+  using adapter = new Adapter({ path: '/org/bluez/hci1' })
   t.is(adapter.path, '/org/bluez/hci1')
-  adapter.destroy()
 })
 
-test('powered is a boolean', { skip: isCI }, (t) => {
-  const adapter = new Adapter()
+test('powered is a boolean', (t) => {
+  using adapter = new Adapter()
   t.is(typeof adapter.powered, 'boolean')
-  adapter.destroy()
 })
 
-test('discovering is a boolean', { skip: isCI }, (t) => {
-  const adapter = new Adapter()
+test('discovering is a boolean', (t) => {
+  using adapter = new Adapter()
   t.is(typeof adapter.discovering, 'boolean')
-  adapter.destroy()
 })
 
-test('address is a MAC string', { skip: isCI }, (t) => {
-  const adapter = new Adapter()
-  t.ok(/^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$/.test(adapter.address))
-  adapter.destroy()
+test('address is a MAC string or null', { skip: isCI }, (t) => {
+  using adapter = new Adapter()
+  t.ok(typeof adapter.address === 'string')
 })
 
 test('set powered toggles value', { skip: isCI }, (t) => {
-  const adapter = new Adapter()
+  using adapter = new Adapter()
   const before = adapter.powered
 
   adapter.powered = !before
@@ -41,8 +36,6 @@ test('set powered toggles value', { skip: isCI }, (t) => {
 
   adapter.powered = before
   t.is(adapter.powered, before)
-
-  adapter.destroy()
 })
 
 test('destroy is idempotent', (t) => {
@@ -53,17 +46,9 @@ test('destroy is idempotent', (t) => {
   })
 })
 
-test('using disposes', (t) => {
-  t.execution(() => {
-    using adapter = new Adapter()
-    t.ok(adapter.path)
-  })
-})
-
-test('inspect shape', { skip: isCI }, (t) => {
-  const adapter = new Adapter()
+test('inspect shape', (t) => {
+  using adapter = new Adapter()
   const obj = adapter[Symbol.for('bare.inspect')]()
   t.ok('path' in obj)
   t.ok('powered' in obj)
-  adapter.destroy()
 })
