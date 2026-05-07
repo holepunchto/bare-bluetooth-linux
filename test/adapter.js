4e23c6d39ccf13d22ec9fd5402cf904aad0d1fbd
Tony Gorez <gorez.tony@gmail.com>
2026-05-07 15:46:31 +0200

    chore: lint
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/adapter.js
+++ test/adapter.js
@@ -1,54 +1,54 @@
-const test = require('brittle')
-const { Adapter } = require('..')
-const { isCI } = require('./helpers')
+const test = require("brittle");
+const { Adapter } = require("..");
+const { isCI } = require("./helpers");
 
-test('constructor sets default path', (t) => {
-  using adapter = new Adapter()
-  t.is(adapter.path, '/org/bluez/hci0')
-})
+test("constructor sets default path", (t) => {
+  using adapter = new Adapter();
+  t.is(adapter.path, "/org/bluez/hci0");
+});
 
-test('constructor accepts custom path', (t) => {
-  using adapter = new Adapter({ path: '/org/bluez/hci1' })
-  t.is(adapter.path, '/org/bluez/hci1')
-})
+test("constructor accepts custom path", (t) => {
+  using adapter = new Adapter({ path: "/org/bluez/hci1" });
+  t.is(adapter.path, "/org/bluez/hci1");
+});
 
-test('powered is a boolean', (t) => {
-  using adapter = new Adapter()
-  t.is(typeof adapter.powered, 'boolean')
-})
+test("powered is a boolean", (t) => {
+  using adapter = new Adapter();
+  t.is(typeof adapter.powered, "boolean");
+});
 
-test('discovering is a boolean', (t) => {
-  using adapter = new Adapter()
-  t.is(typeof adapter.discovering, 'boolean')
-})
+test("discovering is a boolean", (t) => {
+  using adapter = new Adapter();
+  t.is(typeof adapter.discovering, "boolean");
+});
 
-test('address is a MAC string or null', { skip: isCI }, (t) => {
-  using adapter = new Adapter()
-  t.ok(typeof adapter.address === 'string')
-})
+test("address is a MAC string or null", { skip: isCI }, (t) => {
+  using adapter = new Adapter();
+  t.ok(typeof adapter.address === "string");
+});
 
-test('set powered toggles value', { skip: isCI }, (t) => {
-  using adapter = new Adapter()
-  const before = adapter.powered
+test("set powered toggles value", { skip: isCI }, (t) => {
+  using adapter = new Adapter();
+  const before = adapter.powered;
 
-  adapter.powered = !before
-  t.is(adapter.powered, !before)
+  adapter.powered = !before;
+  t.is(adapter.powered, !before);
 
-  adapter.powered = before
-  t.is(adapter.powered, before)
-})
+  adapter.powered = before;
+  t.is(adapter.powered, before);
+});
 
-test('destroy is idempotent', (t) => {
-  const adapter = new Adapter()
+test("destroy is idempotent", (t) => {
+  const adapter = new Adapter();
   t.execution(() => {
-    adapter.destroy()
-    adapter.destroy()
-  })
-})
+    adapter.destroy();
+    adapter.destroy();
+  });
+});
 
-test('inspect shape', (t) => {
-  using adapter = new Adapter()
-  const obj = adapter[Symbol.for('bare.inspect')]()
-  t.ok('path' in obj)
-  t.ok('powered' in obj)
-})
+test("inspect shape", (t) => {
+  using adapter = new Adapter();
+  const obj = adapter[Symbol.for("bare.inspect")]();
+  t.ok("path" in obj);
+  t.ok("powered" in obj);
+});
