177ad56cc00761b51ccf53648a7a3446238e6e09
Tony Gorez <gorez.tony@gmail.com>
2026-05-13 11:29:24 +0200

    test(device): discovery
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/device.js
+++ test/device.js
@@ -1,7 +1,25 @@
 const test = require('brittle')
-const { Device } = require('..')
+const { Adapter, Device } = require('..')
+const { isCI } = require('./helpers')
 
-test('device has expected properties', (t) => {
+test('Device is exported', (t) => {
   t.is(typeof Device, 'function')
   t.is(Device.name, 'Device')
+})
+
+test('device inspect shape', { skip: isCI, timeout: 10000 }, async (t) => {
+  using adapter = new Adapter()
+
+  adapter.startDiscovery()
+
+  const device = await new Promise((resolve) => {
+    adapter.on('device', resolve)
+  })
+
+  adapter.stopDiscovery()
+
+  const obj = device[Symbol.for('bare.inspect')]()
+  t.ok('path' in obj)
+  t.ok('address' in obj)
+  t.ok('name' in obj)
 })
