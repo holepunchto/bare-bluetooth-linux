3900534de3f2f5f57b2c6e7df3ea163d24c754c4
tonygo <tonygo@reversesociety.co>
2026-08-31 15:24:46 +0200

    test: do a powered check before runin test
    Signed-off-by: tonygo <tonygo@reversesociety.co>

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
-require('./test/adapter')
-require('./test/device')
-require('./test/service')
-require('./test/characteristic')
-require('./test/l2cap')
-require('./test/l2cap-vhci')
+const { isCI, poweredAdapter } = require('./test/helpers')
+
+// Every hardware test needs a powered adapter, and would abort on a NotReady
+// throw without one, so stop the suite here rather than at the first test
+if (!isCI && !poweredAdapter()) {
+  console.error('Bluetooth is off on this device, enable it and run again')
+  Bare.exitCode = 1
+} else {
+  require('./test/adapter')
+  require('./test/device')
+  require('./test/service')
+  require('./test/characteristic')
+  require('./test/l2cap')
+  require('./test/l2cap-vhci')
+}
