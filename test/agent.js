e8ab589a9105ea3460ac20cf14a10c773d9712b8
tonygo <tonygo@reversesociety.co>
2026-09-04 16:59:22 +0200

    feat(agent): add base class for consumers
    Signed-off-by: tonygo <tonygo@reversesociety.co>

--- test/agent.js
+++ test/agent.js
@@ -1,11 +1,14 @@
 const test = require('brittle')
-const { Adapter } = require('..')
+const { Adapter, Agent } = require('..')
 const { isCI } = require('./helpers')
 
-const agent = {
-  requestConfirmation() {},
-  requestAuthorization() {}
+class JustWorks extends Agent {
+  requestConfirmation(device, passkey) {}
+
+  requestAuthorization(device) {}
 }
+
+const agent = new JustWorks()
 
 test('registerAgent and unregisterAgent', { skip: isCI }, async (t) => {
   using adapter = new Adapter()
@@ -54,4 +57,28 @@ test('registerAgent rejects an unknown capability', { skip: isCI }, async (t) =>
   using adapter = new Adapter()
 
   await t.exception(() => adapter.registerAgent(agent, 'Telepathy'))
+})
+
+test('Agent is exported', (t) => {
+  t.is(typeof Agent, 'function')
+  t.is(Agent.name, 'Agent')
+})
+
+test('Agent refuses everything it was not taught', (t) => {
+  const bare = new Agent()
+
+  t.exception(() => bare.requestPinCode('/org/bluez/hci0/dev_00'), /not implemented/)
+  t.exception(() => bare.requestPasskey('/org/bluez/hci0/dev_00'), /not implemented/)
+  t.exception(() => bare.requestConfirmation('/org/bluez/hci0/dev_00', 0), /not implemented/)
+  t.exception(() => bare.requestAuthorization('/org/bluez/hci0/dev_00'), /not implemented/)
+  t.exception(() => bare.authorizeService('/org/bluez/hci0/dev_00', '180a'), /not implemented/)
+})
+
+test('Agent ignores what needs no answer', (t) => {
+  const bare = new Agent()
+
+  t.execution(() => bare.displayPinCode('/org/bluez/hci0/dev_00', '0000'))
+  t.execution(() => bare.displayPasskey('/org/bluez/hci0/dev_00', 0, 0))
+  t.execution(() => bare.release())
+  t.execution(() => bare.cancel())
 })
