db5f627639bdf38ced7c8367cadaa57ada82ade9
Tony Gorez <tonygo@reversesociety.co>
2026-09-08 13:19:54 +0200

    feat(adapter): expose agent (#48)
    Signed-off-by: tonygo <tonygo@reversesociety.co>

---
+++ test/agent.js
@@ -0,0 +1,198 @@
+const test = require('brittle')
+const { Adapter, Agent } = require('..')
+const { isCI } = require('./helpers')
+
+class TestAgent extends Agent {
+  requestConfirmation(device, passkey) {}
+
+  requestAuthorization(device) {}
+}
+
+const agent = new TestAgent()
+
+test('registerAgent and unregisterAgent', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+  t.pass('agent registered')
+
+  await adapter.unregisterAgent()
+  t.pass('agent unregistered')
+})
+
+test('registerAgent rejects if already registered', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+
+  await t.exception(() => adapter.registerAgent(agent), /already registered/)
+
+  await adapter.unregisterAgent()
+})
+
+test('unregisterAgent rejects without an agent', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await t.exception(() => adapter.unregisterAgent(), /No agent is registered/)
+})
+
+test('requestDefaultAgent rejects without an agent', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await t.exception(() => adapter.requestDefaultAgent(), /No agent is registered/)
+})
+
+test('requestDefaultAgent after registering', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+
+  await adapter.requestDefaultAgent()
+  t.pass('became the default agent')
+
+  await adapter.unregisterAgent()
+})
+
+test('registerAgent rejects an unknown capability', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await t.exception(() => adapter.registerAgent(agent, 'Telepathy'))
+})
+
+test('registerAgent recovers from a failed registration', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await t.exception(() => adapter.registerAgent(agent, 'Telepathy'))
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+  t.pass('registered after the failure')
+
+  await adapter.unregisterAgent()
+})
+
+test('registerAgent works again after unregistering', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+  await adapter.unregisterAgent()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+  t.pass('registered a second time')
+
+  await adapter.unregisterAgent()
+})
+
+test('destroy releases a registered agent', { skip: isCI }, async (t) => {
+  const adapter = new Adapter()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+
+  t.execution(() => adapter.destroy())
+})
+
+test('registerAgent accepts every capability BlueZ defines', { skip: isCI }, async (t) => {
+  const capabilities = [
+    '',
+    'DisplayOnly',
+    'DisplayYesNo',
+    'KeyboardOnly',
+    'NoInputNoOutput',
+    'KeyboardDisplay'
+  ]
+
+  for (const capability of capabilities) {
+    using adapter = new Adapter()
+
+    await adapter.registerAgent(agent, capability)
+    t.pass('accepted ' + (capability || 'the default'))
+
+    await adapter.unregisterAgent()
+  }
+})
+
+test('registerAgent defaults its capability', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await adapter.registerAgent(agent)
+  t.pass('registered without a capability')
+
+  await adapter.unregisterAgent()
+})
+
+test('unregisterAgent rejects the second time', { skip: isCI }, async (t) => {
+  using adapter = new Adapter()
+
+  await adapter.registerAgent(agent, 'NoInputNoOutput')
+  await adapter.unregisterAgent()
+
+  await t.exception(() => adapter.unregisterAgent(), /No agent is registered/)
+})
+
+test('the agent methods reject once the adapter is destroyed', { skip: isCI }, async (t) => {
+  const adapter = new Adapter()
+  adapter.destroy()
+
+  await t.exception(() => adapter.registerAgent(agent), /destroyed/)
+  await t.exception(() => adapter.requestDefaultAgent(), /destroyed/)
+  await t.exception(() => adapter.unregisterAgent(), /destroyed/)
+})
+
+test('a subclass replaces the refusing defaults', async (t) => {
+  class Answering extends Agent {
+    requestPinCode(device) {
+      return '123456'
+    }
+
+    requestPasskey(device) {
+      return Promise.resolve(42)
+    }
+  }
+
+  const answering = new Answering()
+
+  t.is(answering.requestPinCode('/org/bluez/hci0/dev_00'), '123456')
+  t.is(await answering.requestPasskey('/org/bluez/hci0/dev_00'), 42)
+
+  t.exception(() => answering.requestConfirmation('/org/bluez/hci0/dev_00', 0), /not implemented/)
+})
+
+test('a refusing subclass can return false or throw', (t) => {
+  class Refusing extends Agent {
+    requestConfirmation(device, passkey) {
+      return false
+    }
+
+    requestAuthorization(device) {
+      throw new Error('nope')
+    }
+  }
+
+  const refusing = new Refusing()
+
+  t.is(refusing.requestConfirmation('/org/bluez/hci0/dev_00', 0), false)
+  t.exception(() => refusing.requestAuthorization('/org/bluez/hci0/dev_00'), /nope/)
+})
+
+test('Agent is exported', (t) => {
+  t.is(typeof Agent, 'function')
+  t.is(Agent.name, 'Agent')
+})
+
+test('Agent refuses everything it was not taught', (t) => {
+  const bare = new Agent()
+
+  t.exception(() => bare.requestPinCode('/org/bluez/hci0/dev_00'), /not implemented/)
+  t.exception(() => bare.requestPasskey('/org/bluez/hci0/dev_00'), /not implemented/)
+  t.exception(() => bare.requestConfirmation('/org/bluez/hci0/dev_00', 0), /not implemented/)
+  t.exception(() => bare.requestAuthorization('/org/bluez/hci0/dev_00'), /not implemented/)
+  t.exception(() => bare.authorizeService('/org/bluez/hci0/dev_00', '180a'), /not implemented/)
+  t.exception(() => bare.displayPinCode('/org/bluez/hci0/dev_00', '000000'), /not implemented/)
+})
+
+test('Agent ignores what needs no answer', (t) => {
+  const bare = new Agent()
+
+  t.execution(() => bare.displayPasskey('/org/bluez/hci0/dev_00', 0, 0))
+  t.execution(() => bare.release())
+  t.execution(() => bare.cancel())
+})
